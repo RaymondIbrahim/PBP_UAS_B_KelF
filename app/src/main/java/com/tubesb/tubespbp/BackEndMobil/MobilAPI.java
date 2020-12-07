d192d39ecf4816e92fc4e7173b5794b546624896
RaymondIbrahim <raymondibrahim2000@gmail.com>
2020-12-07 09:51:47 +0700

    master file

--- app/src/main/java/com/tubesb/tubespbp/BackEndMobil/MobilAPI.java
+++ app/src/main/java/com/tubesb/tubespbp/BackEndMobil/MobilAPI.java
@@ -1,7 +1,7 @@
 package com.tubesb.tubespbp.BackEndMobil;
 
 public class MobilAPI {
-    public static final String ROOT_URL   = "http://pbpuasf.000webhostapp.com/";
+    public static final String ROOT_URL   = "https://pbpuasf.000webhostapp.com/";
     public static final String ROOT_API   = ROOT_URL+ "api/";
     public static final String URL_IMAGE  = ROOT_API+"images/";
     public static final String URL_ADD    = ROOT_API+"mobil";
